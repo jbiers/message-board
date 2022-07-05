130edb8edc43441b4c685ac75caafb7221d0c8b8
jbiers <juliabiersuriano@gmail.com>
2022-07-04 22:53:51 -0400

    first messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    creationDate: new Date(),
+  },
+  {
+    text: 'Hello world',
+    user: 'Charles',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messages', messages });
 });
 
 module.exports = router;
