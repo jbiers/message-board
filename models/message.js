eefedad4fe5ce5c4984e4c500ef789a9f5da1c35
jbiers <juliabiersuriano@gmail.com>
2022-07-04 23:26:16 -0400

    added message model

---
+++ models/message.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+    {
+    text: {type: String, required: true, minLength: 1},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    creationDate: {type: Date, default: Date.now, required: true}
+    }
+);
+
+MessageSchema.virtual('formattedCreationDate').get(function() {
+    return  this.creationDate.getDate() + '/' + this.creationDate.getMonth() + '/' + this.creationDate.getFullYear();
+});
+
+MessageSchema.virtual('url').get(function() {
+    return '/message/' + this._id;
+});
+
+export default mongoose.model('Message', MessageSchema);
